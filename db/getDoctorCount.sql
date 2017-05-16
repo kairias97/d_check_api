44fcbd2cf09cf0c23c4e621f2b64082fb22ba865
Kevin Irias <kevin.irias@inaxir.com>
2017-05-16 17:37:25 -0600

    fixed field type lol

--- db/getDoctorCount.sql
+++ db/getDoctorCount.sql
@@ -13,7 +13,15 @@ SELECT
      (SELECT COUNT(sv.*) FROM solicitud_virtual as sv 
      where sv.id_doctor = d.id AND sv.cancelada IS NULL AND sv.aceptada = true) count_consulta_aceptada,
      (SELECT COUNT(sv.*) FROM solicitud_virtual as sv 
-     where sv.id_doctor = d.id AND sv.cancelada = true AND sv.aceptada IS NULL) count_consulta_cancelada
+     where sv.id_doctor = d.id AND sv.cancelada = true AND sv.aceptada IS NULL) count_consulta_cancelada,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = d.id AND sv.cancelada IS NULL AND sv.aceptada IS NULL) count_normal_pendiente,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = d.id AND sv.cancelada IS NULL AND sv.aceptada = false) count_normal_rechazado,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = d.id AND sv.cancelada IS NULL AND sv.aceptada = true) count_normal_aceptado,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = d.id AND sv.cancelada = true AND sv.aceptada IS NULL) count_normal_cancelado
     FROM public.doctor as d
     WHERE id = $1
 GROUP BY id, nombres, apellidos, correo
--- db/getPatientCount.sql
+++ db/getPatientCount.sql
@@ -4,13 +4,21 @@ SELECT
     apellidos, 
     correo,
     (SELECT COUNT(sv.*) FROM solicitud_virtual as sv 
-     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada IS NULL) count_pendiente,
+     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada IS NULL) count_virtual_pendiente,
      (SELECT COUNT(sv.*) FROM solicitud_virtual as sv 
-     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada = false) count_rechazado,
+     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada = false) count_virtual_rechazado,
      (SELECT COUNT(sv.*) FROM solicitud_virtual as sv 
-     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada = true) count_aceptado,
+     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada = true) count_virtual_aceptado,
      (SELECT COUNT(sv.*) FROM solicitud_virtual as sv 
-     where sv.id_paciente = p.id AND sv.cancelada = true AND sv.aceptada IS NULL) count_cancelado
+     where sv.id_paciente = p.id AND sv.cancelada = true AND sv.aceptada IS NULL) count_virtual_cancelado,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada IS NULL) count_normal_pendiente,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada = false) count_normal_rechazado,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = p.id AND sv.cancelada IS NULL AND sv.aceptada = true) count_normal_aceptado,
+     (SELECT COUNT(sv.*) FROM solicitud_presencial as sv 
+     where sv.id_paciente = p.id AND sv.cancelada = true AND sv.aceptada IS NULL) count_normal_cancelado
     FROM public.paciente as p
     WHERE id = $1
 GROUP BY id, nombres, apellidos, correo

7f4ecaca8169c6e35f1b73897ff77beab4de3816
Kevin Irias <kevin.irias@inaxir.com>
2017-05-21 19:44:01 -0600

    Fixed bug with allergies list

--- db/getPatientAllergies.sql
+++ db/getPatientAllergies.sql
@@ -3,7 +3,7 @@ SELECT
     ap.nombre_alergia nombre_alergia_paciente,
     CAST(ap.id_paciente AS INT) id_paciente,
     CAST(ca.id AS INT) id_categoria_alergia,
-    ca.nombre_categoria_alergia nombre_categoria_alerta
+    ca.nombre_categoria_alergia nombre_categoria_alergia
 FROM alergia_paciente AS ap
 JOIN categoria_alergia AS ca ON ap.id_categoria_alergia = ca.id
 WHERE ap.id_paciente = $1;
